67adb5548ec1a1e1ad8f3faaf397cc6dfcc8503b
Sameel Nawaz <sameelnawaz@gmail.com>
2022-02-21 12:10:40 +0500

    Meta files

---
+++ Assets/Demo/Scripts/CharacterAppearance.cs
@@ -0,0 +1,25 @@
+using FishNet.Object;
+using UnityEngine;
+
+public class CharacterAppearance : NetworkBehaviour
+{
+    [SerializeField]
+    private Material m_playerMat;
+    [SerializeField]
+    private Material m_opponentMat;
+
+    [SerializeField]
+    private MeshRenderer m_MeshRenderer;
+
+    public override void OnStartClient()
+    {
+        //base.OnStartClient();
+        m_MeshRenderer.material = base.IsOwner ? m_playerMat : m_opponentMat;
+
+        if (!base.IsOwner)
+        {
+            transform.position = Vector3.Reflect(transform.position, Vector3.forward);
+        }
+    }
+
+}
